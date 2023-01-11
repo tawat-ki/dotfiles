819744e5ba7c31594e43c69da2e5d96ffaa0e301
tawat-ki <tawat_ki@kkumail.com>
2023-01-12 00:23:32 +0700

    all the things

--- .config/nvim/lua/deboost/packer.lua
+++ .config/nvim/lua/deboost/packer.lua
@@ -4,36 +4,36 @@
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
-	-- Packer can manage itself
-	use("wbthomason/packer.nvim")
-	use("Mofiqul/dracula.nvim")
-	use("xiyaowong/nvim-transparent")
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	})
-	use("lukas-reineke/indent-blankline.nvim")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-nvim-lua")
-	use("hrsh7th/nvim-cmp")
-	use("saadparwaiz1/cmp_luasnip")
-	use("neovim/nvim-lspconfig")
-	use("hrsh7th/cmp-cmdline")
-	use("mhartington/formatter.nvim")
-	use("ThePrimeagen/vim-be-good")
-	use("untitled-ai/jupyter_ascending.vim")
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = function()
-			require("nvim-treesitter.install").update({ with_sync = true })
-		end,
-	})
+  -- Packer can manage itself
+  use("wbthomason/packer.nvim")
+  use("Mofiqul/dracula.nvim")
+  use("xiyaowong/nvim-transparent")
+  use({
+    "nvim-telescope/telescope.nvim",
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true },
+  })
+  use("lukas-reineke/indent-blankline.nvim")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/cmp-path")
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-nvim-lua")
+  use("hrsh7th/nvim-cmp")
+  use("saadparwaiz1/cmp_luasnip")
+  use("neovim/nvim-lspconfig")
+  use("hrsh7th/cmp-cmdline")
+  use("mhartington/formatter.nvim")
+  use("ThePrimeagen/vim-be-good")
+  use("untitled-ai/jupyter_ascending.vim")
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = function()
+      require("nvim-treesitter.install").update({ with_sync = true })
+    end,
+  })
   use("nvim-treesitter/nvim-treesitter-context")
   use({
     "iamcco/markdown-preview.nvim",
